0590db97a74b77d015da23e0982e54dfa234b1db
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2021-04-19 16:05:30 +0200

    drum

---
+++ Intec projecten/myfirstscript.js
@@ -0,0 +1,19 @@
+
+console.log('hello world!');
+
+let a = 15;
+let b = 16;
+
+let difference = a - b;
+
+console.log('difference of ' + a + ' and ' + b + ' is '+ difference)
+
+a = true;
+console.log('what happend to a? what is its value now? it is:' + a)
+
+
+
+funciton helloworldfunction()
+{
+
+}
